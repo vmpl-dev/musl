fd301dd31f6d4d0320c0abc3a7272cb985f439bd
Benshan Mei <mbs2021@163.com>
2023-12-13 04:10:45 +0800

    Add comments

--- src/thread/x86_64/syscall_cp.s
+++ src/thread/x86_64/syscall_cp.s
@@ -16,15 +16,15 @@ __cp_begin:
 	mov (%rdi),%eax
 	test %eax,%eax
 	jnz __cp_cancel
-	mov %rdi,%r11
-	mov %rsi,%rax
-	mov %rdx,%rdi
-	mov %rcx,%rsi
-	mov %r8,%rdx
-	mov %r9,%r10
-	mov 8(%rsp),%r8
-	mov 16(%rsp),%r9
-	mov %r11,8(%rsp)
+	mov %rdi,%r11		/* save rdi[arg0] to r11[tmp] */
+	mov %rsi,%rax		/* save rsi[arg1] to rax[sys_nr] */
+	mov %rdx,%rdi		/* save rdx[arg2] to rdi[arg0] */
+	mov %rcx,%rsi		/* save rcx[arg3] to rsi[arg1] */
+	mov %r8,%rdx		/* save r8[arg4] to rdx[arg2] */
+	mov %r9,%r10		/* save r9[arg5] to r10[arg3] */
+	mov 8(%rsp),%r8		/* save [arg6] to r8[arg4] */
+	mov 16(%rsp),%r9	/* save [arg7] to r9[arg5] */
+	mov %r11,8(%rsp)	/* restore r11[tmp] to [arg6] */
 	VMPL_SYSCALL
 __cp_end:
 	ret
